c000ba85a55ec5145dfa2bba65a0aab78467af0e
MaherAhmed0 <maher7ahmed985@gmail.com>
2022-12-12 19:22:46 +0200

    Done preprocessing TRAIN & TEST data.

--- Preprocessing.py
+++ Preprocessing.py
@@ -4,6 +4,7 @@ import cv2
 import os
 import imghdr
 import random
+from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 import pickle
 from PIL import Image
@@ -11,7 +12,7 @@ import re
 import tensorflow as tf
 
 
-def create_preprocessed_data(data, path):
+def preprocessed_train_data(data, path):
     converted_images = 0
     for category in CATEGORIES:
         data_path = os.path.join(path, category)
@@ -37,12 +38,13 @@ def create_preprocessed_data(data, path):
             # print("Shape: ", img_array.shape)
 
             # Smoothing
-            img_array = cv2.GaussianBlur(img_array, (3, 3), 0)
+            # img_array = cv2.GaussianBlur(img_array, (3, 3), 0)
 
             # Normalization
             img_array = (img_array - np.min(img_array)) / (np.max(img_array) - np.min(img_array))
             ###############################
             data.append([img_array, label])
+            # break
             # if c == 2:
             #     break
             # c += 1
@@ -51,12 +53,36 @@ def create_preprocessed_data(data, path):
         print("------------------------------")
         # break
     print(converted_images, "images got converted from PNG to JPG")
+    print("Total train images:", len(data))
     print("------------------------------")
 
 
-def show_image(x, y, index):
+def preprocessed_test_data(data, path):
+    converted_images = 0
+    print("loading test images")
+    for img in tqdm(os.listdir(path)):
+        test_img_array = cv2.imread(os.path.join(path, img))
+        if imghdr.what(os.path.join(path, img)) == 'png':
+            cv2.imwrite(os.path.join(path, img), test_img_array, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+            os.remove(os.path.join(path, img))
+            converted_images += 1
+        test_img_array = cv2.cvtColor(test_img_array, cv2.COLOR_BGR2RGB)
+        test_img_array = cv2.resize(test_img_array, (50, 50))
+        data.append(test_img_array)
+    print("Done.")
+    print(converted_images, "images got converted from PNG to JPG")
+    print("Total test images:", len(data))
+    print("------------------------------")
+
+
+def show_train_image(x, y, index):
     plt.imshow(x[index], cmap='gray')
     plt.xlabel(CATEGORIES[y[index]])
+    plt.show()
+
+
+def show_test_image(arr, index):
+    plt.imshow(arr[index], cmap='gray')
     plt.show()
 
 
@@ -67,13 +93,16 @@ def retrieve_pickled_data(file_name):
     return data
 
 
-Data_path = "D:\\Courses\\Neural Network and Deep Learning\\Project\\Sports-Image-Classification\\try"
+Train_Data_Path = "D:\\Courses\\Neural Network and Deep Learning\\Project\\Sports-Image-Classification\\try"
+Test_Data_Path = "D:\\Courses\\Neural Network and Deep Learning\\Project\\Sports-Image-Classification\\test"
+
 CATEGORIES = ["Basketball", "Football", "Rowing", "Swimming", "Tennis", "Yoga"]
 Train_Data = []
+Test_Data = []
 
-create_preprocessed_data(Train_Data, Data_path)
-print("Total images:", len(Train_Data))
-print("------------------------------")
+preprocessed_train_data(Train_Data, Train_Data_Path)
+preprocessed_test_data(Test_Data, Test_Data_Path)
+show_test_image(Test_Data, 0)
 
 random.shuffle(Train_Data)
 X_Train_Data = []
@@ -83,12 +112,7 @@ for img_data, img_label in Train_Data:
     X_Train_Data.append(img_data)
     Y_Train_Data.append(img_label)
 
-show_image(X_Train_Data, Y_Train_Data, 0)
-show_image(X_Train_Data, Y_Train_Data, 5)
-show_image(X_Train_Data, Y_Train_Data, 10)
-show_image(X_Train_Data, Y_Train_Data, 15)
-show_image(X_Train_Data, Y_Train_Data, 20)
-show_image(X_Train_Data, Y_Train_Data, 25)
+show_train_image(X_Train_Data, Y_Train_Data, 0)
 
 # print(Train_Data)
 # print(X_Train_Data)
@@ -104,9 +128,13 @@ pickle.dump(Y_Train_Data, pickle_out)
 pickle_out.close()
 del Y_Train_Data
 
+pickle_out = open("Test images", "wb")
+pickle.dump(Test_Data, pickle_out)
+pickle_out.close()
+del Test_Data
+
 X_Train = retrieve_pickled_data("X_Train")
 Y_Train = retrieve_pickled_data("Y_Train")
+Test = retrieve_pickled_data("Test images")
 
-print(X_Train)
-print(Y_Train)
-
+x_train, x_test, y_train, y_test = train_test_split(X_Train, Y_Train, train_size=0.8)
